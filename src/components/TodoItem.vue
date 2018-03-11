c9189f0e0a5cfd8cd1537175f69d14a466f442e3
Luis Vilanova <luisvilanovarodriguez@gmail.com>
2018-03-11 17:48:54 +0100

    check ico added

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="todoWrapper">
-        <span v-on:click="toggleCompleted" v-bind:class="[{completed: task.completed}, 'icons']">
+        <span v-on:click="toggleCompleted" v-bind:class="{checkicon: task.completed}"></span>
+        <span v-on:click="toggleCompleted" v-bind:class="[{completed: task.completed},'task']">
             {{task.title}}
+
         </span> 
-        <button class="removeTodo" v-on:click="removeItem">delete</button>
+        <button class="removeTodo" v-on:click="removeItem">X</button>
 
     </div>
 </template>
